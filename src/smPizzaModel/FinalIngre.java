8656196b75dcd6fb511ac7b8a9a82223e196d007
kiranSadananda <kiran.sadananda1994@gmail.com>
2020-04-21 21:26:26 -0400

    update few corrections

--- src/smPizzaModel/FinalIngre.java
+++ src/smPizzaModel/FinalIngre.java
@@ -33,7 +33,6 @@ public class FinalIngre extends ConditionalActivity {
 
     @Override
     protected void terminatingEvent() {
-        // TODO: Needs to check for correctness
         model.qSlide.add(iCPizza);
         model.rqMakeTable.position[MakeTable.POS5] = null;
         model.rqMakeTable.numBusy--;
